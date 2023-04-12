e343078540687fbccf757b6e75d8e290ca47582f
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2023-04-12 17:10:03 -0300

    feat: add coroutine methods

--- src/main/kotlin/br/com/car/adapters/api/CarResource.kt
+++ src/main/kotlin/br/com/car/adapters/api/CarResource.kt
@@ -2,6 +2,7 @@ package br.com.car.adapters.api
 
 import br.com.car.domain.model.Car
 import br.com.car.domain.ports.CarService
+import kotlinx.coroutines.runBlocking
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -19,6 +20,12 @@ class CarResource(
     fun update(@RequestBody car: Car, @PathVariable id: Long) = carService.update(car, id)
 
     @GetMapping("/lista-modelo")
-    fun listByInventory(@RequestParam model: String) = carService.listByInventory(model)
+    fun listByInventory(@RequestParam model: String) =
+    /** Com runBlocking abrimos uma requisição suspensa [suspend fun],
+     * Quer dizer que começa a virtual thread requisição, mas não necessariamente termina
+     * Outra Virtual Thread pode terminar a requisição **/
+        runBlocking {
+            carService.listByInventory(model)
+        }
 }
 
